b5f03ab79a51e8d2a0c5ecf649bf2dff13cfc733
Genis Lleopart <genisllm@gmail.com>
2018-03-28 19:22:43 +0200

    Rows per Processador

---
+++ Parallel/Code/rows_per_proc_module.f90
@@ -0,0 +1,21 @@
+module rows_per_proc_module
+use mpi
+implicit none
+
+subroutine rows_per_proc(nPart,myFirstPart,myLastPart)
+implicit none
+integer, intent(IN)                       :: nPart
+integer, intent(OUT)                      :: myFirstPart, myLastPart
+integer                                   :: numPart, rank, numProcs, ierror
+
+call mpi_comm_rank(mpi_comm_world, rank, ierror)
+call mpi_comm_size(mpi_comm_world, numProcs, ierror)
+
+numPart = nPart/numProcs
+myFirstPart = rank*numPart + 1
+myLastPart = myFirstPart + numPart - 1
+if (rank == numProcs - 1) myLastPart = nPart
+
+end subroutine rows_per_proc
+
+end module rows_per_proc_module
