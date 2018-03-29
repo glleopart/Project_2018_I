c10888d1694debfcb9ebd2d912546c7b95122c22
Genis Lleopart <genisllm@gmail.com>
2018-03-29 16:14:57 +0200

    Kinetic Modificada

--- Parallel/Code/kinetic_energy_module.f90
+++ Parallel/Code/kinetic_energy_module.f90
@@ -1,11 +1,11 @@
 module kinetic_energy_module
 use mpi
 contains
-subroutine kinetic_energy(vel, KETotal, myFirstPart, myLastPart, nPart)
+subroutine kinetic_energy(vel, KETotal, Tinst, myFirstPart, myLastPart, nPart)
 implicit none
 integer, intent(in)                             :: nPart, myFirstPart, myLastPart
 real(8), dimension(nPart,3), intent(in)         :: vel
-real(8), intent(out)                            :: KETotal
+real(8), intent(out)                            :: KETotal, Tinst
 real(8), dimension(3)                           :: vec
 real(8)                                         :: modV, KE
 integer                                         :: i, ierror
@@ -20,6 +20,9 @@ end do
 
 
 call mpi_reduce(KE, KETotal, 1, MPI_real8, MPI_SUM, rMaster, mpi_comm_world, ierror)
+KETotal = KETotal*0.5
+Tinst = 2.0*KETotal/(3.0*float(nPart))
+
 
 end subroutine kinetic_energy
 end module kinetic_energy_module
