f72e595cc156576a662aa7a5aa3ef7a7b8e10f84
jorch28 <jsdfiguero@gmail.com>
2018-03-29 16:22:30 +0200

    subrutina verlet mpi

---
+++ Parallel/Code/mpi_verlet_module.f90
@@ -0,0 +1,53 @@
+module mpi_verlet_module
+use mpi
+use lj_module
+use pbc_module
+contains
+    subroutine vel_verlet(time,dt, pos, vel, nPart, eps, sig, boxSize,cutOff,V,F_aux,myFirstPart,myLastPart)
+    integer,intent(in)         :: nPart,rank,ierror,myFirstPart,myLastPart
+    integer :: i,j,k
+    integer,parameter :: rmaster = 0
+    real(8),dimension(myFirstPart:myLastPart,3),intent(inout)        :: vel,pos,F 
+    real(8),intent(in),dimension(myFirstPart:myLastPart,3)           :: F_aux 
+    real(8),dimension(3)         :: vec
+    real(8),intent(inout)        :: time
+    real(8),intent(out)          :: V
+    real(8),intent(in)           :: dt, eps,sig,boxsize,cutOff
+    real(8),allocatable          :: full_dats
+        if ((time == 0).and.(rank == rmaster)) then 
+            call LJ_pot(nPart, pos, eps, sig, boxSize, cutOff,F_aux, V)
+        endif
+        if (time == 0) then
+            do i = myFirstPart , myLastPart, 1
+                vec(:) = pos(i,:) + vel(i,:)*dt + F_aux(i,:)*dt**3./2.
+                call pbc(vec,boxsize)
+                pos(i,:) = vec(:)
+            end do
+        endif
+        if ((time /= 0).and.(rank == rmaster)) then
+            F_aux(:,:) = F(:,:)
+            call LJ_pot(nPart, pos, eps, sig, boxSize, cutOff,F, V)
+        endif
+        if (time/=0) then 
+            do i = myFirstPart,myLastPart, 1
+                vel(i,:) = vel(i,:)*dt +(F_aux(i,:)+ F(i,:))*dt/2.
+            end do
+        endif
+        call send_recv_array(F,myFirstPart,myLastPart,rank,nRow,status,full_dats) 
+        
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    end subroutine
+end module 
