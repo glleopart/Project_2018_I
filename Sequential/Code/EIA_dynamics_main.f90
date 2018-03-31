1dd8af3fac0e8cb74da161cb2e178a776b49b9c3
Genis Lleopart <genisllm@gmail.com>
2018-03-31 18:22:26 +0200

    Temps en el sequential

--- Sequential/Code/EIA_dynamics_main.f90
+++ Sequential/Code/EIA_dynamics_main.f90
@@ -25,6 +25,12 @@ real(8)                                 :: V, eps, sig, time, KE, Tinst
 real(8), allocatable, dimension(:)      :: total_momentum
 integer                                 :: seed, trjCount, thermCount
 integer                                 :: initUn, finUn, trajUn, dataUn, velUn, paramUn
+real(4)                                 :: printIniT,  printFinT,  printTotT
+real(4)                                 :: verletIniT, verletFinT, verletTotT, verletTime
+real(4)                                 :: thermoIniT, thermoFinT, thermoTotT
+real(4)                                 :: forcesIniT, forcesFinT, forcesTotT
+real(4)                                 :: momentIniT, momentFinT, momentTotT
+real(4)                                 :: kinetiIniT, kinetiFinT, kinetiTotT
     
 call cpu_time(start)
 call get_command_argument(1, fName, status=fStat)
@@ -65,6 +71,7 @@ open(unit=dataUn, file='data.out')              ! T, Ken, V, temps...
 open(unit=velUn,  file='velocity.out')          ! T, Ken, V, temps...
 open(unit=paramUn,file='parameters.out')        ! Parameters needed for statistics
                                                 ! (MB and RDF)
+write(dataUn,*), '# time, V, KE, KE + V, mom, Tins'
 write(paramUn,*) nSteps/100 - 1, nPart
 write(paramUn,*) boxSize
 
@@ -75,18 +82,45 @@ time = 0.0D0; trjCount = 0; thermCount = 0
 call print_positions(trajUn, nPart, pos, time)
 call print_positions(velUn,  nPart, vel, time)
 
+! Posem temps a zero
+verletTotT = 0.0
+thermoTotT = 0.0
+printTotT  = 0.0
+forcesTotT = 0.0
+momentTotT = 0.0
+kinetiTotT = 0.0
+
+
 do i = 1, nSteps, 1
-        call velocity_verlet(time, dt, pos, vel, nPart, eps, sig, boxSize, cutOff, V, F)
+        call cpu_time(verletIniT)
+        call velocity_verlet(time, dt, pos, vel, nPart, eps, sig, boxSize, cutOff, V, F,forcesTotT)
+        call cpu_time(verletFinT)
+        verletTotT = verletTotT + (verletFinT - verletIniT)
         if (mod(trjCount,100) == 0) then
-                call print_positions(trajUn, nPart, pos, time)
-                call print_positions(velUn,  nPart, vel, time)
+                call cpu_time(kinetiIniT)
                 call kinetic_energy(vel, KE, Tinst, nPart)
+                call cpu_time(kinetiFinT)
+                kinetiTotT = kinetiTotT + (kinetiFinT - kinetiIniT)
+                
+                call cpu_time(momentIniT)
                 call momentum(nPart, vel, total_momentum)
+                call cpu_time(momentFinT)
+                momentTotT = momentTotT + (momentFinT - momentIniT)
+                
+                call cpu_time(printIniT)
+                call print_positions(trajUn, nPart, pos, time) 
+                call print_positions(velUn,  nPart, vel, time)
                 call print_data(time, V, KE, Tinst, total_momentum, dataUn)
+                call cpu_time(printFinT)
+                printTotT = printTotT + (printFinT - printIniT)
+
                 trjCount = 0
         end if
         if (mod(thermCount,10) == 0) then
+                call cpu_time(thermoIniT)
                 call andersen_thermo(dt, T, nPart, i, vel)
+                call cpu_time(thermoFinT)
+                thermoTotT = thermoTotT + (thermoFinT - thermoIniT)
                 thermCount = 0
         end if
         trjCount = trjCount + 1
@@ -96,7 +130,23 @@ call print_positions(finUn, nPart, pos, time)
 
 
 call cpu_time(finish)
-write(dataUn,*) "# CPU TIME: ", finish - start
+verletTime = verletTotT - forcesTotT
+        write(dataUn,*) '# VERLET TOTAL_CPU_TIME  : ', verletTime
+        write(dataUn,*) '# THERMO TOTAL_CPU_TIME  : ', thermoTotT
+        write(dataUn,*) '# FORCES TOTAL_CPU_TIME  : ', forcesTotT
+        write(dataUn,*) '# MOMENT TOTAL_CPU_TIME  : ', momentTotT
+        write(dataUn,*) '# KINETIC TOTAL_CPU_TIME : ', kinetiTotT
+        write(dataUn,*) '# PRINTING TIME : ', printTotT
+        write(dataUn,*) "# TOTAL CPU TIME         : ", finish - start
+        write(dataUn,*)
+        write(dataUn,*)
+        write(dataUn,*) '# RELATIVE TIME TO THE TOTAL CPU_TIME (IN %):'
+        write(dataUn,*) '# VERLET   : ', 100*verletTime/(finish-start)
+        write(dataUn,*) '# THERMO   : ', 100*thermoTotT/(finish-start)
+        write(dataUn,*) '# FORCES   : ', 100*forcesTotT/(finish-start)
+        write(dataUn,*) '# MOMENT   : ', 100*momentTotT/(finish-start)
+        write(dataUn,*) '# KINETIC  : ', 100*kinetiTotT/(finish-start)
+        write(dataUn,*) '# PRINTING : ', 100*printTotT/(finish-start)
 close(un); close(initUn); close(finUn); close(trajUn); close(dataUn)
 contains
 
--- Sequential/Code/vel_verlet_module.f90
+++ Sequential/Code/vel_verlet_module.f90
@@ -2,18 +2,26 @@ module vel_verlet_module
 use pbc_module
 use lj_module
 contains
-subroutine velocity_verlet(time, dt, pos, vel, nPart, eps, sig, boxSize, cutOff, V, F)
+subroutine velocity_verlet(time, dt, pos, vel, nPart, eps, sig, boxSize, cutOff, V, F, forcesTotT)
 implicit none
 integer, intent(in)                             :: nPart
 real(8), intent(in)                             :: dt, eps, sig, boxSize, cutOff
 real(8), intent(inout)                          :: time
 real(8), intent(out)                            :: V
 real(8), dimension(nPart,3), intent(inout)      :: pos, vel, F
+real(4), intent(in)                             :: forcesTotT
 real(8), dimension(nPart,3)                     :: F_aux
 integer                                         :: i, j, k
 real(8), dimension(3)                           :: vec
+real(4)                                         :: forcesIniT, forcesFinT
+
 !Starting the simulation the forces are obtained calling LJ_pot
-if (time == 0) call LJ_pot(nPart, pos, eps, sig, boxSize, cutOff, F_aux, V)
+if (time == 0) then
+         if (present(forcesTotT)) call cpu_time(forcesIniT)
+         call LJ_pot(nPart, pos, eps, sig, boxSize, cutOff, F_aux, V)
+         if (present(forcesTotT)) call cpu_time(forcesFinT)
+         if (present(forcesTotT)) forcesTotT = forcesTotT + (forcesFinT - forcesIniT)
+end if
 !Once the simulation is running the forces enter as an input value F(t)
 !Also F(t+dt) is the output of the subroutine
 if (time /= 0) F_aux(:,:) = F(:,:)
@@ -26,7 +34,10 @@ do i = 1, nPart, 1
         pos(i,:) = vec(:)
 end do
 !Calculation of forces at time t+dt
+if (present(forcesTotT)) call cpu_time(forcesIniT)
 call LJ_pot(nPart, pos, eps, sig, boxSize, cutOff, F, V)
+if (present(forcesTotT)) call cpu_time(forcesFinT)
+if (present(forcesTotT)) forcesTotT = forcesTotT + (forcesFinT - forcesIniT)
 
 !calculation of vel(t+dt)
 do i = 1, nPart, 1
